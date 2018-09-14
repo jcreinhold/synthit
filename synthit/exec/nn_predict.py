WIP: testing mixed precision, fix cuda on prediction

synthit/exec/nn_predict.py
@@ -73,14 +73,13 @@ def main(args=None):
     logger = logging.getLogger(__name__)
     try:
         # set torch to use cuda if available (and desired) and set number of threads for the CPU (if enabled)
-        dev_str = "cuda" if torch.cuda.is_available() and not args.disable_cuda else "cpu"
-        device = torch.device(dev_str)
+        device = torch.device("cuda" if torch.cuda.is_available() and not args.disable_cuda else "cpu")
         torch.set_num_threads(args.n_jobs)
 
         # load the trained model
         if no_config_file:
             logger.warning('Loading entire serialized model in non-preferred way (without config file)')
-            model = torch.load(args.trained_model, map_location=dev_str)
+            model = torch.load(args.trained_model)
         else:
             if args.nn_arch == 'nconv':
                 from synthit.models.nconvnet import Conv3dNLayerNet
@@ -92,8 +91,12 @@ def main(args=None):
                              activation=args.activation, output_activation=args.out_activation, use_up_conv=args.use_up_conv)
             else:
                 raise SynthError(f'Invalid NN type: {args.nn_arch}. {{nconv, unet}} are the only supported options.')
-            model.load_state_dict(torch.load(args.trained_model, map_location=dev_str))
+            model.load_state_dict(torch.load(args.trained_model))
         logger.debug(model)
+
+        # put the model on the GPU if available and desired
+        if torch.cuda.is_available() and not args.disable_cuda:
+            model.cuda()
 
         # set convenience variables and grab filenames of images to synthesize
         psz = model.patch_sz
@@ -130,7 +133,7 @@ def main(args=None):
                 out_img_ants = img_ants.new_image_like(out_img / count_mtx)
             else:
                 test_img_t = torch.from_numpy(img).to(device)[None, None, ...]
-                out_img = np.squeeze(model.forward(test_img_t).data.numpy())
+                out_img = np.squeeze(model.forward(test_img_t).cpu().data.numpy())
                 out_img_ants = img_ants.new_image_like(out_img)
             out_fn = args.predict_out + str(k) + '.nii.gz'
             out_img_ants.to_file(out_fn)

synthit/exec/nn_train.py
@@ -92,6 +92,8 @@ def arg_parser():
                             help='type of activation to use throughout network except output [Default=relu]')
     nn_options.add_argument('-oac', '--out-activation', type=str, default='linear', choices=('relu', 'lrelu', 'linear'),
                             help='type of activation to use in network on output [Default=linear]')
+    nn_options.add_argument('-mp', '--fp16', action='store_true', default=False,
+                            help='enable mixed precision training')
     nn_options.add_argument('--disable-cuda', action='store_true', default=False,
                             help='Disable CUDA regardless of availability')
     return parser
@@ -114,6 +116,15 @@ def main(args=None):
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=level)
     logger = logging.getLogger(__name__)
     try:
+        # import and initialize mixed precision training package
+        amp_handle = None
+        if args.fp16:
+           try:
+               from apex import amp
+               amp_handle = amp.init()
+           except ImportError:
+               logger.info('Mixed precision training (i.e., the package `apex`) not available.')
+
         # set number of threads if using CPU
         torch.set_num_threads(args.n_jobs)
 
@@ -149,6 +160,7 @@ def main(args=None):
         # define dataset and split into training/validation set
         dataset = NiftiImageDataset(args.source_dir, args.target_dir, crop=crop, disable_cuda=args.disable_cuda)
 
+        # setup training and validation set
         num_train = len(dataset)
         indices = list(range(num_train))
         split = args.validation_count
@@ -181,7 +193,11 @@ def main(args=None):
 
                 # Zero gradients, perform a backward pass, and update the weights.
                 optimizer.zero_grad()
-                loss.backward()
+                if args.fp16 and amp_handle is not None:
+                    with amp_handle.scale_loss(loss, optimizer) as scaled_loss:
+                        scaled_loss.backward()
+                else:
+                    loss.backward()
                 optimizer.step()
             train_losses.append(losses)
 